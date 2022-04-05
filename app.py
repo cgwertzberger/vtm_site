7333a2ddf531f01fa1e732443f2745912080a890
Chase Wertzberger <78705246+cgwertzberger@users.noreply.github.com>
2022-04-04 23:04:10 -0500

    tried to fix calculations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for  
+from flask import Flask
+from flask import request, render_template, redirect, url_for  
 
 app = Flask(__name__)
 
@@ -14,31 +15,23 @@ def about():
 def estimate():
     return render_template('estimate.html')
 
-@app.route('/math', methods=['POST'])
-def math():
-        if request.method == 'POST':
-            form = request.form
-            radius = int(form['radius'])
-            height = int(form['height'])
-            pi = float(3.14)
-            print(radius)
-            print(height)
-            Top_Area = pi*(radius)**2
-            print(Top_Area)
-            Side_Area = 2*(pi*(radius*height))
-            print(Side_Area)
-            Total_Area = Top_Area+Side_Area
-            print(Total_Area)
-            Total_AreaSQF = Total_Area/144
-            print(Total_AreaSQF)
-            Material_Cost = Total_AreaSQF*25
-            print(Material_Cost)
-            Labor_Cost = Total_AreaSQF*15
-            print(Labor_Cost)
-            Total_Cost = Material_Cost + Labor_Cost
-            print(Total_Cost)
-            return render_template('estimate.html')
-        return render_template('estimate.html')
+@app.route('/estimate', methods=['GET', 'POST'])
+def estimate():
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        pi = 3.14
+        top = pi * radius**2
+        sides = 2 * (pi * (radius * height))
+        area = top + sides
+        sqft = area / 144
+        material = sqft * 25
+        labor = sqft * 15
+        estimate_total = "${:,.2f}".format((round(material + labor, 2)))
+        return render_template('estimate.html', quote=estimate_total)
+    return render_template('estimate.html', pageTitle="Get an Estimate")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
