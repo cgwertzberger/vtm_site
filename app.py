de9e43cf391a5291bdc8c6a077841722e0b53f7a
Chase Wertzberger <78705246+cgwertzberger@users.noreply.github.com>
2022-04-04 22:14:07 -0500

    Updated Base templated and tried to calculate in app.py

--- app.py
+++ app.py
@@ -1,38 +1,44 @@
-from __future__ import with_statement
-from functools import singledispatch
-from unicodedata import numeric
-from flask import Flask
-from flask import render_template, redirect, request, url_for
+from flask import Flask, request, render_template, redirect, url_for  
 
 app = Flask(__name__)
 
-friend_list = [{"name": "Chase Wertzberger" } ]
-
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Vertical Tank Maintenance')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', pageTitle='About Us')
+    return render_template('about.html')
 
-@app.route('/estimate', methods=['GET', 'POST'])
+@app.route('/estimate')
 def estimate():
-    if request.method == 'POST':
-        form = request.form
-        radius = float(form['radius'])
-        height = float(form['height'])
-        pi = 3.14
-        top = pi * radius**2
-        sides = 2 * (pi * (radius * height))
-        area = top + sides
-        sqft = area / 144
-        material = sqft * 25
-        labor = sqft * 15
-        estimate = "${:,.2f}".format((round(material + labor, 2)))
-        return render_template('estimate.html', quote=estimate)
-    return render_template('estimate.html', pageTitle="Get an Estimate")
+    return render_template('estimate.html')
 
+@app.route('/math', methods=['POST'])
+def math():
+        if request.method == 'POST':
+            form = request.form
+            radius = int(form['radius'])
+            height = int(form['height'])
+            pi = float(3.14)
+            print(radius)
+            print(height)
+            Top_Area = pi*(radius)**2
+            print(Top_Area)
+            Side_Area = 2*(pi*(radius*height))
+            print(Side_Area)
+            Total_Area = Top_Area+Side_Area
+            print(Total_Area)
+            Total_AreaSQF = Total_Area/144
+            print(Total_AreaSQF)
+            Material_Cost = Total_AreaSQF*25
+            print(Material_Cost)
+            Labor_Cost = Total_AreaSQF*15
+            print(Labor_Cost)
+            Total_Cost = Material_Cost + Labor_Cost
+            print(Total_Cost)
+            return render_template('estimate.html')
+        return render_template('estimate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
